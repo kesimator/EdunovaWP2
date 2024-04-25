f5656517b90551f1d38c0a992c3ce9c2e59d42d8
kesimator <132385476+kesimator@users.noreply.github.com>
2024-04-25 13:15:03 +0200

    Problem riješen, SVE RADI!

--- Frontend/src/services/httpService.js
+++ Frontend/src/services/httpService.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const httpService = axios.create({
     baseURL: 'https://mkesinovi-001-site1.ctempurl.com/api/v1',
     headers: {
-        'Content-Type': 'application.json'
+        'Content-Type': 'application/json'
     }
 });
